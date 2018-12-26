901b04b7c9e699ccd5d21b95309231473cf54abd
jinsholic77@gmail.com <ace21315!>
2018-12-26 10:37:15 +0900

    Initial commit for rx-java

---
+++ README.md
@@ -0,0 +1,6 @@
+# start rx-java
+RxJava를 활용한 리액티브 프로그래밍 실습과 테스트 프로젝트
+
+* stack
+ - jdk v1.8.0
+ - spring-boot v2.1.1
---
+++ src/main/java/com/rhyno/rx/RxApplication.java
@@ -0,0 +1,14 @@
+package com.rhyno.rx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxApplication.class, args);
+	}
+
+}
+
